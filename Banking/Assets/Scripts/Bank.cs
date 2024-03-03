8f0f8465a678c4920285b11dbc6e0f9a28abb5fd
vaibhavyxx <vd5443@rit.edu>
2024-03-02 23:59:18 -0500

    credit and debit card

---
+++ Banking/Assets/Scripts/Bank.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bank : MonoBehaviour
+{
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            panel.SetActive(true);
+            Debug.Log("true");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            panel.SetActive(false);
+            Debug.Log("true");
+        }
+    }
+}
---
+++ Banking/Assets/Scripts/Credit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    //public GameObject player;
+
+    void AddCredit()
+    {
+        //player.
+        //Adds a credit
+    }
+}
